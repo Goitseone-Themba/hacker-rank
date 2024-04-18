98526f0f6cfc66449b5e7c1704bcfc251dbd7c56
Goitseone_Themba <97857682+Goitseone-Themba@users.noreply.github.com>
2024-04-18 22:17:55 +0200

    first question in the algorithm problem set

---
+++ src/algorithms/solveMeFirst.java
@@ -0,0 +1,13 @@
+package algorithms;//@author: Goitseone Themba 21000539
+import java.util.Scanner;
+public class solveMeFirst {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        System.out.println(a + b);
+    }
+}
+
